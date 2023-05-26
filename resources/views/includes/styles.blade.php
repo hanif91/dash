73af39af87f09e770cf718fbe2e02ee9f8d8c6c3
hanif kurniawan <hans.thereddevil@gmail.com>
2023-05-26 14:11:05 +0700

    step copy dashboard blank

---
+++ resources/views/includes/styles.blade.php
@@ -0,0 +1,7 @@
+
+<link rel="stylesheet" href="{{ asset('assets/css/style.css')}}"/>
+
+<link rel="stylesheet" href="{{ asset('assets/libs/jquery-nice-select/css/nice-select.css') }}" />
+
+<link rel="stylesheet" href="{{ asset('assets/libs/nouislider/nouislider.min.css')}}"/>
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="admin, dashboard">
+	<meta name="author" content="DexignZone">
+	<meta name="robots" content="index, follow">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Dompet : Payment Admin Template">
+	<meta property="og:title" content="Dompet : Payment Admin Template">
+	<meta property="og:description" content="Dompet : Payment Admin Template">
+	<meta property="og:image" content="https://dompet.dexignlab.com/xhtml/social-image.png">
+	<meta name="format-detection" content="telephone=no">
+
+	<!-- PAGE TITLE HERE -->
+	<title>Dashboard</title>
+
+	<!-- FAVICONS ICON -->
+	{{-- <link rel="shortcut icon" type="image/png" href="images/favicon.png"> --}}
+
+	{{-- <link href="vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
+	<link rel="stylesheet" href="vendor/nouislider/nouislider.min.css">
+
+    <link href="css/style.css" rel="stylesheet"> --}}
+	<!-- Style css -->
+    @include('includes.styles')
+</head>
+
+<body>
+    <div id="main-wrapper">
+        @yield('content')
+
+        @include('includes.header')
+
+
+        @include('includes.sidebar')
+
+
+        @include('includes.sidebar')
+    </div>
+
+    @include('includes.scripts')
+	<script>
+		jQuery(document).ready(function(){
+			setTimeout(function() {
+				dezSettingsOptions.version = 'dark';
+				new dezSettings(dezSettingsOptions);
+			},500)
+		});
+	</script>
+</body>
+</html>
